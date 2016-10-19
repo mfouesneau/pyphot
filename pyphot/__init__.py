207159e51466cecb0de5c0775463ef1c1a2687d6
Timothy Morton <tim.morton@gmail.com>
2016-10-19 18:34:30 -0400

    uncommented line

--- pyphot/__init__.py
+++ pyphot/__init__.py
@@ -1,6 +1,6 @@
 from .phot import (Library, Ascii_Library, HDF_Library, Filter, get_library)
 from .licks import (LickLibrary, LickIndex, reduce_resolution)
-# from .ezunits import unit
+from .ezunits import unit
 from .helpers import (STmag_from_flux, STmag_to_flux, extractPhotometry,
                       extractSEDs, fluxErrTomag, fluxToMag, magErrToFlux,
                       magToFlux)
