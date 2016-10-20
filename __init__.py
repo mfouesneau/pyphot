7f904c8c71dac641960f0f5f14be8facabf395a4
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2016-10-20 09:39:30 -0400

    Cleaning

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .pyphot import Library, Ascii_Library, HDF_Library, Filter, get_library, unit
-from .pyphot import (LickLibrary, LickIndex, reduce_resolution)
-from .pyphot.helpers import (STmag_from_flux, STmag_to_flux, extractPhotometry,
-                             extractSEDs, fluxErrTomag, fluxToMag,
-                             magErrToFlux, magToFlux)
