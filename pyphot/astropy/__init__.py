3b9444299c7eaa07540354aca7f268c25c7eb6f3
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2019-11-06 16:37:28 +0100

    pep8

--- pyphot/astropy/__init__.py
+++ pyphot/astropy/__init__.py
@@ -3,11 +3,11 @@
 from astropy.units import Unit, def_unit, add_enabled_units
 
 new_units = dict(
-    flam = 'erg * s ** (-1) * AA ** (-1) * cm **(-2)',
-    fnu = 'erg * s ** (-1) * Hz ** (-1) * cm **(-2)',
-    photflam = 'photon * s ** (-1) * AA ** (-1) * cm **(-2)',
-    photfnu = 'photon * s ** (-1) * Hz ** (-1) * cm **(-2)',
-    angstroms = 'angstrom'
+    flam='erg * s ** (-1) * AA ** (-1) * cm **(-2)',
+    fnu='erg * s ** (-1) * Hz ** (-1) * cm **(-2)',
+    photflam='photon * s ** (-1) * AA ** (-1) * cm **(-2)',
+    photfnu='photon * s ** (-1) * Hz ** (-1) * cm **(-2)',
+    angstroms='angstrom'
 )
 
 add_enabled_units([def_unit([k], Unit(v)) for k, v in new_units.items()])\
