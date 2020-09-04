943b2a5e35c88b4419d5e93a613e44f22994eae3
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2020-09-04 12:13:30 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name = "pyphot",
-    version = 1.0,
+    version = 1.1,
     description = "A tool for computing photometry from spectra",
     long_description = readme(),
     author = "Morgan Fouesneau",
