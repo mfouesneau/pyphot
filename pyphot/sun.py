31bf54ddc0289499c77574138b5e6fc4f5cb3f39
Morgan Fouesneau <fouesneau@mpia.de>
2025-01-21 11:18:26 +0100

    formatter

--- pyphot/sun.py
+++ pyphot/sun.py
@@ -1,28 +1,30 @@
-""" Handle the Sun Spectrum
+"""Handle the Sun Spectrum"""
 
-"""
 from __future__ import print_function
 from functools import wraps
 import numpy
 from .config import libsdir
 from .simpletable import SimpleTable
 from .ezunits import unit
-try: 
+
+try:
     from astropy.io import fits as pyfits
 except ImportError:
     import pyfits
 
 
-__all__ = ['Sun']
+__all__ = ["Sun"]
 
-_default_sun = {'observed': "{0}/sun_reference_stis_001.fits".format(libsdir), 
-                'theoretical': "{0}/sun_kurucz93.fits".format(libsdir)}
-_default_distance = '1 * au'
+_default_sun = {
+    "observed": "{0}/sun_reference_stis_001.fits".format(libsdir),
+    "theoretical": "{0}/sun_kurucz93.fits".format(libsdir),
+}
+_default_distance = "1 * au"
 
 
 class Sun(object):
     """
-    Class that handles the Sun's spectrum and references.  
+    Class that handles the Sun's spectrum and references.
 
     Observed solar spectrum comes from:
     ftp://ftp.stsci.edu/cdbs/current_calspec/sun_reference_stis_001.fits
@@ -36,8 +38,8 @@ class Sun(object):
     parameters when the Sun is at 1 au.
 
      log_Z         T_eff        log_g           V_{Johnson}
-     +0.0           5777        +4.44              -26.75 
-    
+     +0.0           5777        +4.44              -26.75
+
     Attributes
     ----------
     source: str
@@ -56,23 +58,27 @@ class Sun(object):
         either 'observed' using the stis reference,
         or  'theoretical' for the Kurucz model.
     """
-    def __init__(self, source=None, distance=1 * unit['au'], flavor='theoretical'):
-        """ Constructor """
+
+    def __init__(self, source=None, distance=1 * unit["au"], flavor="theoretical"):
+        """Constructor"""
         self.data = None
         self.units = None
         self.distance = distance
-        self.distance_conversion = 1.
+        self.distance_conversion = 1.0
         self._set_source_flavor(source, flavor)
 
     def _set_source_flavor(self, source, flavor):
-        if not flavor.lower() in _default_sun:
-            raise RuntimeError('Flavor must be either theoretical or observed')
+        if flavor.lower() not in _default_sun:
+            raise RuntimeError("Flavor must be either theoretical or observed")
         self.flavor = flavor
         if source is not None:
             self.source = source
         else:
             self.source = _default_sun[flavor]
-            self.distance_conversion = ((unit[_default_distance].to(str(self.distance.units)) / self.distance) ** 2).magnitude
+            self.distance_conversion = (
+                (unit[_default_distance].to(str(self.distance.units)) / self.distance)
+                ** 2
+            ).magnitude
             self.data = None
             self.units = None
 
@@ -83,33 +89,33 @@ class Sun(object):
         # get extension
         self.data = SimpleTable(fname, silent=True)
         try:
-            self.data.header['WAVELENGTH_UNIT'] = self.data._units['WAVELENGTH']
-            self.data.header['FLUX_UNIT'] = self.data._units['FLUX']
-            self.units = self.data._units['WAVELENGTH'], self.data._units['FLUX']
+            self.data.header["WAVELENGTH_UNIT"] = self.data._units["WAVELENGTH"]
+            self.data.header["FLUX_UNIT"] = self.data._units["FLUX"]
+            self.units = self.data._units["WAVELENGTH"], self.data._units["FLUX"]
         except AttributeError:
             pass
 
         try:
-            uw = self.data.header['WAVELENGTH_UNIT'].split('=')[0].rstrip()
-            uf = self.data.header['FLUX_UNIT'].split('=')[0].rstrip()
+            uw = self.data.header["WAVELENGTH_UNIT"].split("=")[0].rstrip()
+            uf = self.data.header["FLUX_UNIT"].split("=")[0].rstrip()
             self.units = uw, uf
         except TypeError:
-            uw = self.data.header['WAVELENGTH_UNIT'].split(b'=')[0].decode().rstrip()
-            uf = self.data.header['FLUX_UNIT'].split(b'=')[0].decode().rstrip()
+            uw = self.data.header["WAVELENGTH_UNIT"].split(b"=")[0].decode().rstrip()
+            uf = self.data.header["FLUX_UNIT"].split(b"=")[0].decode().rstrip()
             self.units = uw, uf
 
     def __enter__(self):
-        """ Enter context """
+        """Enter context"""
         self._readfile()
         return self
 
-    def __exit__(self,  *exc_info):
-        """ end context """
+    def __exit__(self, *exc_info):
+        """end context"""
         return False
 
     @property
     def wavelength(self):
-        """ wavelength (with units when found) """
+        """wavelength (with units when found)"""
         self._readfile()
         try:
             return self.data.WAVELENGTH * unit[self.units[0].lower()]
@@ -118,9 +124,11 @@ class Sun(object):
 
     @property
     def flux(self):
-        """ flux(wavelength) values (with units when provided) """
+        """flux(wavelength) values (with units when provided)"""
         self._readfile()
         try:
-            return self.data.FLUX * unit[self.units[1].lower()] * self.distance_conversion
+            return (
+                self.data.FLUX * unit[self.units[1].lower()] * self.distance_conversion
+            )
         except:
             return self.data.FLUX * self.distance_conversion
