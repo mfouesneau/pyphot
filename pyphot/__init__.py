5e93404141cbef51abc722a6febd8575dc647e6f
Morgan Fouesneau <fouesneau@mpia.de>
2025-10-31 18:21:18 +0100

    fix inconsistency

--- pyphot/__init__.py
+++ pyphot/__init__.py
@@ -1,4 +1,7 @@
-# from .phot import Library, Ascii_Library, HDF_Library, Filter
+from typing import Optional
+
+from pyphot.future.unit_adapters.units_adapter import UnitsAdapter
+
 from .licks import LickLibrary, LickIndex, reduce_resolution
 
 from .sandbox import (
@@ -12,25 +15,12 @@ from .sandbox import (
 
 from .sandbox import get_library
 from .ezunits import unit
-# from .helpers import (
-#     STmag_from_flux,
-#     STmag_to_flux,
-#     extractPhotometry,
-#     extractSEDs,
-#     fluxErrTomag,
-#     fluxToMag,
-#     magErrToFlux,
-#     magToFlux,
-# )
+
 from .vega import Vega
 from .sun import Sun
 from .version import __VERSION__
 
 __all__ = [
-    # Library,
-    # Ascii_Library,
-    # HDF_Library,
-    # Filter,
     LickLibrary,
     LickIndex,
     reduce_resolution,
@@ -42,14 +32,6 @@ __all__ = [
     UnitLickLibrary,
     get_library,
     unit,
-#     STmag_from_flux,
-#     STmag_to_flux,
-#     extractPhotometry,
-#     extractSEDs,
-#     fluxErrTomag,
-#     fluxToMag,
-#     magErrToFlux,
-#     magToFlux,
     Vega,
     Sun,
     __VERSION__,
--- pyphot/config.py
+++ pyphot/config.py
@@ -1,16 +1,34 @@
-from __future__ import print_function
 import os
 import inspect
+from dataclasses import dataclass
+from importlib import resources
 
 
-#directories
-__ROOT__ = '/'.join(os.path.abspath(inspect.getfile(inspect.currentframe())).split('/')[:-1])
+# directories
+__ROOT__ = "/".join(
+    os.path.abspath(inspect.getfile(inspect.currentframe())).split("/")[:-1]  # type: ignore
+)
 # default library directory
 # libsdir = os.path.abspath(os.path.join(__ROOT__, '../libs/'))
-# from pkg_resources import resource_filename
-# libsdir = resource_filename('pyphot', 'libs')
-from importlib import resources
-libsdir = resources.files('pyphot').joinpath('libs')
+libsdir = resources.files("pyphot").joinpath("libs")
+
+# default passband library
+__default_passband_lib__ = libsdir.joinpath("new_filters.hd5")
+__default_lick_lib__ = libsdir.joinpath("licks.dat")
 
 # Set default vega flavor
-__vega_default_flavor__ = 'stis_003'
+__vega_default_flavor__ = "stis_003"
+
+
+def set_vega_flavor(flavor: str):
+    """Set the vega flavor to use throughout pyphot.
+
+    Parameters
+    ----------
+    flavor : str
+        The name of the vega flavor to use.
+
+    see :py:mod:`pyphot.vega`
+    """
+    global __vega_default_flavor__
+    __vega_default_flavor__ = flavor
